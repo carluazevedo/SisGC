6e253dc4df4c33b788465a83cf5bbe282e3b816a
Carlu P. Azevedo <carluazevedo@gmail.com>
2016-07-18 05:49:15 -0300

    Correção arquivo de configuração para banco de dados

--- application/config/database.php
+++ application/config/database.php
@@ -74,7 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'mysql:host=localhost;dbname=sisgc;charset=UTF8',
+	'dsn'	   => 'mysql:host=localhost;dbname=sisgc;charset=UTF8',
 	'hostname' => 'localhost',
 	'username' => 'mysql_user',
 	'password' => '',
@@ -88,7 +88,7 @@ $db['default'] = array(
 	'char_set' => 'utf8',
 	'dbcollat' => 'utf8_general_ci',
 	'swap_pre' => '',
-	'encrypt' => FALSE,
+	'encrypt'  => FALSE,
 	'compress' => FALSE,
 	'stricton' => FALSE,
 	'failover' => array(),
